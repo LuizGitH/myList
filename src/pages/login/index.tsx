2644e2410fa72e84b0d97008723b4a29c671c1c9
Luiz <luis2016felip@gmail.com>
2026-06-11 22:12:22 -0300

    segundo commit

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,11 +1,27 @@
 import React from "react";
 
 import {
-    Text
+  Text,
+  View,
+  Image
 } from 'react-native';
+import { style } from "./styles";
+import Logo from '../../assets/logo.png';
 
-export default function Login (){
-    return (
-        <Text> Ola mundo! </Text>
-    )
+export default function Login() {
+  return (
+    <View style={style.container}>
+      <View style={style.boxTop}>
+        <Image
+          source={Logo}
+        />
+      </View>
+      <View style={style.boxMid}>
+        <Text>mid</Text>
+      </View>
+      <View style={style.boxBottom}>
+        <Text>bottom</Text>
+      </View>
+    </View>
+  );
 }
