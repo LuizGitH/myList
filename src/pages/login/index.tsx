4bbda18fed24e34fa71d59eb3635b55ad1b1e547
Luiz <luis2016felip@gmail.com>
2026-06-12 20:25:36 -0300

    terceiro commit

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,27 +1,50 @@
 import React from "react";
 
 import {
-  Text,
-  View,
-  Image
-} from 'react-native';
-import { style } from "./styles";
-import Logo from '../../assets/logo.png';
+    Text,
+    View,
+    Image,
+    TextInput
+} from "react-native";
 
-export default function Login() {
-  return (
-    <View style={style.container}>
-      <View style={style.boxTop}>
-        <Image
-          source={Logo}
-        />
-      </View>
-      <View style={style.boxMid}>
-        <Text>mid</Text>
-      </View>
-      <View style={style.boxBottom}>
-        <Text>bottom</Text>
-      </View>
-    </View>
-  );
+import { style } from "./styles";
+import Logo from "../../assets/logo.png";
+import { MaterialIcons } from "@expo/vector-icons";
+import { themes } from "../../global/themes";
+
+export default function Login (){
+    return (
+        <View style={style.container}>
+            <View style={style.boxTop}>
+                <Image
+                    source={Logo}
+                    style={style.logo}
+                    resizeMode="contain"
+                />
+                <Text style={style.text}>Bem vindo de volta!</Text>
+            </View>
+
+            <View style={style.boxMid}>
+                <Text style={style.titleInput}>ENDEREÇO E-MAIL</Text>
+
+                <View style={style.BoxInput}>
+                    <TextInput
+                        style={style.input}
+                    />
+
+                    <MaterialIcons
+                        name='email'
+                        size={20}
+                        color={themes.colors.gray}
+                    />
+                </View>
+
+                <Text style={style.titleInput}>SENHA</Text>
+                <TextInput />
+            </View>
+
+            <View style={style.boxBottom}>
+            </View>
+        </View>
+    )
 }
--- src/pages/login/styles.ts
+++ src/pages/login/styles.ts
@@ -1,4 +1,5 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+import { themas } from "../../global/themes";
 
 export const style = StyleSheet.create({
   container: {
@@ -6,19 +7,53 @@ export const style = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   boxTop: {
-    height: 200,
-    width:'100%',
-    backgroundColor: 'red'
+    height: Dimensions.get('window').height / 3,
+    width: '100%',
+    //backgroundColor: 'red',
+    alignItems: 'center',
+    justifyContent: 'center'
   },
+
   boxMid: {
-    height: 200,
-    width:'100%',
-    backgroundColor: 'green'
+    height: Dimensions.get('window').height / 4,
+    width: '100%',
+    backgroundColor: 'green',
+    paddingHorizontal:37
   },
+
   boxBottom: {
-    height: 200,
-    width:'100%',
+    height: Dimensions.get('window').height / 3,
+    width: '100%',
     backgroundColor: 'blue'
+  },
+  logo:{
+    width:80,
+    height:80
+  },
+  text:{
+    fontWeight:'bold',
+    marginTop:40,
+    fontSize:18
+  },
+  titleInput:{
+    marginLeft:5,
+    color:themas.colors.gray,
+    marginTop:20
+  },
+  BoxInput:{
+    width:'100%',
+    height:40,
+    borderWidth: 1,
+    borderRadius: 40,
+    marginTop: 10,
+    flexDirection: 'row'
+  },
+  input:{
+    height:'100%',
+    width:'90%',
+    backgroundColor:'red',
+    borderRadius: 40
   }
 });
